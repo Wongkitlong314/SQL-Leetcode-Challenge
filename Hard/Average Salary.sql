4e3b92c5ba0f5213bb98a98394be6ac48c656f1b
Wongkitlong314 <78060157+Wongkitlong314@users.noreply.github.com>
2024-03-03 10:37:44 +0800

    Update Average Salary.sql

--- Hard/Average Salary.sql
+++ Hard/Average Salary.sql
@@ -63,3 +63,41 @@ else "lower"
 end as comparison
 from t1
 order by 1 desc
+
+
+
+
+
+
+
+
+
+
+
+CREATE TABLE employee (
+  employee_id INT PRIMARY KEY,
+  department_id INT
+);
+
+INSERT INTO employee (employee_id, department_id) VALUES
+(1, 1),
+(2, 2),
+(3, 2);
+
+-- Create the table
+CREATE TABLE salary (
+id INT PRIMARY KEY,
+employee_id INT,
+amount DECIMAL(10,2),
+pay_date DATE
+);
+
+-- Insert the data
+INSERT INTO salary (id, employee_id, amount, pay_date) VALUES
+(1, 1, 9000, '2017-03-31'),
+(2, 2, 6000, '2017-03-31'),
+(3, 3, 10000, '2017-03-31'),
+(4, 1, 7000, '2017-02-28'),
+(5, 2, 6000, '2017-02-28'),
+(6, 3, 8000, '2017-02-28');
+
